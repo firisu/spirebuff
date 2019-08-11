3324ba5fb59395c22d9e0c2fd5871205da100389
firisu <firisu@gmail.com>
2019-08-11 21:07:42 +0900

    ウォッチ用ファイルコミット

---
+++ watch.js
@@ -0,0 +1,40 @@
+process.env.NODE_ENV = "development";
+
+const fs = require("fs-extra");
+const paths = require("react-scripts/config/paths");
+const webpack = require("webpack");
+const config = require("react-scripts/config/webpack.config.js")("production");
+
+var entry = config.entry;
+var plugins = config.plugins;
+
+entry = entry.filter(fileName => !fileName.match(/webpackHotDevClient/));
+plugins = plugins.filter(
+  plugin => !(plugin instanceof webpack.HotModuleReplacementPlugin)
+);
+
+config.entry = entry;
+config.plugins = plugins;
+config.target = "electron-renderer";
+config.optimization.minimize = false;
+
+webpack(config).watch({}, (err, stats) => {
+  if (err) {
+    console.error(err);
+  } else {
+    copyPublicFolder();
+  }
+  console.error(
+    stats.toString({
+      chunks: false,
+      colors: true
+    })
+  );
+});
+
+function copyPublicFolder() {
+  fs.copySync(paths.appPublic, paths.appBuild, {
+    dereference: true,
+    filter: file => file !== paths.appHtml
+  });
+}
