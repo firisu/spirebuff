db97be39e44fdd85bed2bab2ea4fa6ece74bd055
firisu <firisu@gmail.com>
2019-08-11 15:24:55 +0900

    開発用、ビルド用の設定分け

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -1,4 +1,6 @@
 const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const url = require("url");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -15,7 +17,14 @@ function createWindow() {
   });
 
   // and load the index.html of the app.
-  win.loadURL("http://localhost:3000/");
+  const startURL = process.env.ELECTRON_DEV
+    ? "http://localhost:3000"
+    : url.format({
+        pathname: path.join(__dirname, "/../build/index.html"),
+        protocol: "file:",
+        slashes: true
+      });
+  win.loadURL(startURL);
 
   // Open the DevTools.
   win.webContents.openDevTools();
