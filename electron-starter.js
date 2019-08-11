e4478652c0764043ad048e06520ba3f3b121d2d2
firisu <firisu@gmail.com>
2019-08-11 20:47:22 +0900

    常にローカルから index.html を読み込むようにした

--- electron-starter.js
+++ electron-starter.js
@@ -16,11 +16,7 @@ function createWindow() {
   });
 
   // and load the index.html of the app.
-  if (process.env.LOCAL_DEVELOPMENT === "true") {
-    win.loadURL("http://localhost:3000");
-  } else {
-    win.loadFile(path.join(__dirname, "./build/index.html"));
-  }
+  win.loadFile(path.join(__dirname, "./build/index.html"));
 
   // Open the DevTools.
   win.webContents.openDevTools();
