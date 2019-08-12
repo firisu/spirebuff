3551f19a97e32b22e00a0eb399f33eca05a46946
firisu <firisu@gmail.com>
2019-08-12 20:12:51 +0900

    見出しのサイズを少し変更

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ const App: React.FC = () => {
         </Grid.Row>
         <Grid.Row>
           <Grid.Column width={10}>
-            <Header inverted size="tiny">
+            <Header inverted size="small">
               最新のプレイ({LATEST_RUNS_COUNT}件)
             </Header>
             <RunTable runs={latestRuns} />
