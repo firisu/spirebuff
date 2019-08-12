25d38edb4394691c859ab9a88ac54b3a6b980ecc
firisu <firisu@gmail.com>
2019-08-12 13:57:13 +0900

    スタート時の横幅増加

--- electron-starter.js
+++ electron-starter.js
@@ -8,7 +8,7 @@ let win;
 function createWindow() {
   // Create the browser window.
   win = new BrowserWindow({
-    width: 1200,
+    width: 1400,
     height: 1000,
     webPreferences: {
       nodeIntegration: true
