ffc08902fb8359657ac0f26899884b0ea290877a
firisu <firisu@gmail.com>
2019-08-12 13:18:54 +0900

    reset.css を指定するようにした

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,8 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 
 import App from "./App";
+import "./reset.css";
 import "./index.css";
-import * as serviceWorker from "./serviceWorker";
 import reducer from "./rootReducer";
 
 const store = createStore(reducer);
@@ -16,8 +16,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
