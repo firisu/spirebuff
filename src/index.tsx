d6d2aa6bf059d1022bbf604caace4ac090921061
firisu <firisu@gmail.com>
2019-08-17 14:28:23 +0900

    カラー用スタイルファイル定義

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import reducer from "./rootReducer";
 // styles
 import "./index.css";
 import "semantic-ui-css/semantic.min.css";
-import "./semantic-ui.override.scss";
+import "./styles/index.css";
 
 const store = createStore(reducer);
 
