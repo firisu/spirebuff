85e98c9b5d7d0d2d63eadd14e944194570224f45
firisu <firisu@gmail.com>
2019-08-27 12:07:45 +0900

    モンスター, UIのローカリ追加

--- src/modules/localization.ts
+++ src/modules/localization.ts
@@ -6,6 +6,10 @@ import relics_jpn from "localization/jpn/relics.json";
 import relics_eng from "localization/eng/relics.json";
 import events_jpn from "localization/jpn/events.json";
 import events_eng from "localization/eng/events.json";
+import monsters_jpn from "localization/jpn/monsters.json";
+import monsters_eng from "localization/eng/monsters.json";
+import ui_jpn from "localization/jpn/ui.json";
+import ui_eng from "localization/eng/ui.json";
 
 export interface CardLocalization {
   NAME: string;
@@ -36,27 +40,53 @@ export interface EventsJson {
   [id: string]: EventLocalization;
 }
 
+export interface MonstersJson {
+  [id: string]: {
+    MOVES: ReadonlyArray<string>;
+    NAME: string;
+    DIALOG?: ReadonlyArray<string>;
+  };
+}
+
+export interface UiJson {
+  RunHistoryMonsterNames: {
+    TEXT: ReadonlyArray<string>;
+  };
+}
+
 export type GameLanguage = "jpn" | "eng";
 
 export const useLocalization = () => {
   const language = React.useContext(LanguageContext);
 
-  const jsons: { cards: CardsJson; relics: RelicsJson; events: EventsJson } = {
+  const jsons: {
+    cards: CardsJson;
+    relics: RelicsJson;
+    events: EventsJson;
+    monsters: MonstersJson;
+    ui: UiJson;
+  } = {
     cards: {},
     relics: {},
-    events: {}
+    events: {},
+    monsters: {},
+    ui: { RunHistoryMonsterNames: { TEXT: [] } }
   };
   switch (language) {
     case "jpn": {
       jsons.cards = cards_jpn;
       jsons.relics = relics_jpn;
       jsons.events = events_jpn;
+      jsons.monsters = monsters_jpn;
+      jsons.ui = ui_jpn;
       break;
     }
     case "eng": {
       jsons.cards = cards_eng;
       jsons.relics = relics_eng;
       jsons.events = events_eng;
+      jsons.monsters = monsters_eng;
+      jsons.ui = ui_eng;
       break;
     }
   }
@@ -79,7 +109,9 @@ export const useLocalization = () => {
         throw new Error(`${id} のイベント情報が見つかりませんでした。`);
       }
       return jsons.events[id];
-    }
+    },
+    monstersJson: jsons.monsters,
+    uiJson: jsons.ui
   };
 };
 
